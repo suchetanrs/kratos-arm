063639f0a63115c21d93f1f40594350923606130
suchetanrs <79915569+suchetanrs@users.noreply.github.com>
2021-11-12 01:57:53 +0530

    Create manualcontrol.py

---
+++ presentation/2arduinos/manualcontrol.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+
+import rospy
+from std_msgs.msg import *
+from sensor_msgs.msg import *
+from geometry_msgs.msg import *
+
+def cb(data):
+    global joy_value
+    print("ok1")
+    joy_value1=data.axes[3]
+    joy_value2=data.axes[4]
+    value_scaled1=joy_value1
+    value_scaled2=joy_value2
+
+    obj = Twist()
+    obj.linear.x=value_scaled1
+    obj.linear.y=value_scaled2
+    pub.publish(obj)
+
+rospy.init_node("joyinput",anonymous=True)
+pub=rospy.Publisher("cmd_vel",Twist,queue_size=10)
+print("ok")
+sub=rospy.Subscriber("joy", Joy, cb)
+rospy.spin()
