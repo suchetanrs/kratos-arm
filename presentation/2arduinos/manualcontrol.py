4aa70cb7a8705c2eef858304061140bfb3109199
suchetanrs <79915569+suchetanrs@users.noreply.github.com>
2021-11-12 01:59:27 +0530

    Update manualcontrol.py

--- presentation/2arduinos/manualcontrol.py
+++ presentation/2arduinos/manualcontrol.py
@@ -19,7 +19,7 @@ def cb(data):
     pub.publish(obj)
 
 rospy.init_node("joyinput",anonymous=True)
-pub=rospy.Publisher("cmd_vel",Twist,queue_size=10)
+pub=rospy.Publisher("actuators",Twist,queue_size=10)
 print("ok")
 sub=rospy.Subscriber("joy", Joy, cb)
 rospy.spin()
